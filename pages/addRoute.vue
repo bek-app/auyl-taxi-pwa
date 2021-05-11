8af53aa9207a4f0fb4802ca701fe21e37ace666e
bek-app <make202006@gmail.com>
2021-05-11 09:00:50 +0600

    created post request

--- pages/addRoute.vue
+++ pages/addRoute.vue
@@ -1,16 +1,44 @@
 <template>
   <b-container class="container">
-    <iframe
+    <!-- <iframe
       class="mt-2"
       src="https://www.google.com/maps/d/embed?mid=1TPhXvmotMAUOpO3fqw9G02fKulDeFrmz"
       width="100%"
       height="600"
-    ></iframe>
+    ></iframe> -->
+
+    <b-form-input
+      v-model="directionFrom"
+      placeholder="Қай жерден?"
+    ></b-form-input>
+    <b-form-input v-model="directionTo" placeholder="Қай жаққа?"></b-form-input>
+    <b-form-input v-model.number="price" placeholder="Бағасы"></b-form-input>
+    <b-button block variant="warning" @click="sendOrder">Жіберу</b-button>
   </b-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      directionTo: '',
+      directionFrom: '',
+      price: null,
+    }
+  },
+  mounted() {},
+  methods: {
+    sendOrder() {
+      if (!this.directionTo || !this.directionFrom || !this.price) return
+      const payload = {
+        directionTo: this.directionTo,
+        directionFrom: this.directionFrom,
+        price: this.price,
+      }
+      this.$store.dispatch('addroute/createOrder', payload)
+    },
+  },
+}
 </script>
 <style scoped>
 .container {
